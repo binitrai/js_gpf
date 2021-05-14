6da86e3106e99ac670d3cff57e28cdab479c1051
Binit Rai <binitcse@gmail.com>
2021-05-14 11:51:00 +0530

    Create strLowerUpperCase.js

---
+++ strLowerUpperCase.js
@@ -0,0 +1,13 @@
+function lowerCase(str) {
+    if (typeof str === "string") {
+        return str.toLowerCase();
+    }
+    return str;
+}
+
+function upperCase(str) {
+    if (typeof str === "string") {
+        return str.toUpperCase();
+    }
+    return str;
+}
